190f24a5661df2d01db1d839c4bd679f80d3c578
Suprith KG <suprith7kg@gmail.com>
2024-02-12 00:55:05 +0530

    url change

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express()
 
 app.use(express.json())
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://guileless-jalebi-432852.netlify.app/", credentials: true }));
 
 var allowCrossDomain = function(req, res, next) {
     // res.header('Access-Control-Allow-Origin', "*");
