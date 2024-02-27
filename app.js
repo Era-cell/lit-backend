467e072ddcea1062a6ea622b8b45dde905866078
Suprith <suprith7kg@gmail.com>
2024-02-27 23:37:21 +0530

    ci/cd

--- app.js
+++ app.js
@@ -67,12 +67,21 @@ const ensureOriginConfigured = async () => {
     }
 };
 
+// Remove .git/index.lock if it exists
+const removeIndexLock = async () => {
+    try {
+        await git.raw(['rm', '-f', '.git/index.lock']);
+    } catch (error) {
+        console.error(`Error removing index.lock: ${error.message}`);
+    }
+};
+
 setInterval(async () => {
     try {
         // serialized_agent[0][1] = ["hi", "hi"];
         // console.log(serialized_agent)
         // Read the file content
-
+        removeIndexLock()
         await ensureOriginConfigured();
         const newContent = `export const model = ${util.inspect(serialized_agent, { depth: null })};`;
 
